9e40696f2785d8dde83e3be8dccc3ee901930102
AiLeo0557 <leo1949@dingtalk.com>
2025-05-09 10:39:01 +0800

    feat: 添加 tsup 配置文件并更新 .gitignore

---
+++ tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entryPoints: ['src/index.ts'],
+  dts: true,
+  clean: true,
+  format: 'esm',
+  splitting: true,
+  sourcemap: false,
+  minify: true
+})
